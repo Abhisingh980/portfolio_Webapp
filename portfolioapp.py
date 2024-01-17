0ea75480b4cfdbb56661e22cb551790906d4c3b3
Abhisingh980 <kr.abhinesh147@gmail.com>
2024-01-17 09:37:21 +0530

    first web abb development part 1 add title add description image and url

--- portfolioapp.py
+++ portfolioapp.py
@@ -9,31 +9,40 @@ with col1:
     st.image("my img.jpg")
 
 
-content = '''Hi,i am Abhinesh! i am a python programmer,developer, an founder of my self.I graduated in 2023
+content1 = '''Hi,i am Abhinesh! i am a python programmer,developer, an founder of my self.I graduated in 2023
  with Master in computer application ,focus on using python for remote sensing.i have worked with companies from a
  various countries ,such as center for conservation Geography,to map and understand australian ecosystems  ,
  image processing with the swiss in terra ,and performing data mining to gain business insights with australian rapid 
   intelligence'''
 
-content = content.title()
+content1 = content1.title()
 with col2:
     st.title(" Abhinesh kumar")
-    st.info(content)
+    st.info(content1)
 
-content = "Below You can find some of the apps I have built in pythin . feel free to contact me"
 
-st.write(content)
+col6, col7 = st.columns(2)
+content2 = "Below You can find some of the apps I have built in pythin . feel free to contact me"
+with col6:
+    st.write(content2)
+
 
 df = pd.read_csv("data.csv", sep=";")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 
 
